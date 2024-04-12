6f8731d86924350066861840e2cf8773a6605ddc
Frisaf <fridasafsten@gmail.com>
2024-04-12 11:21:06 +0200

    started

---
+++ thegame.py
@@ -0,0 +1,9 @@
+print("You wake up in a wagon. What happened? You don't remember.")
+wagon_choice = input("The wagon seems to stand still at the moment. What do you do?\n1. Peek outside.\n2. Stay in the wagon.\n3. Shout to see if anybody is there.")
+print(wagon_choice)
+if wagon_choice == 1:
+    print("You take a look outside of the wooden wagon, the bright light of the afternoon sun blinding you. It seems like you are currently on a big, grass covered field.")
+    print("You can't see anything other than the endless field of grass anywhere. What do you do?")
+elif wagon_choice == 3:
+    print("No one hears you.")
+    print(wagon_choice)
