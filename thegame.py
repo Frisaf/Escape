7cb5856d20cd87f1210480006903e63bf834e910
Frisaf <fridasafsten@gmail.com>
2024-04-15 11:16:30 +0200

    tried to get started, got the if loop to work

--- thegame.py
+++ thegame.py
@@ -1,9 +1,23 @@
-print("You wake up in a wagon. What happened? You don't remember.")
-wagon_choice = input("The wagon seems to stand still at the moment. What do you do?\n1. Peek outside.\n2. Stay in the wagon.\n3. Shout to see if anybody is there.")
-print(wagon_choice)
-if wagon_choice == 1:
-    print("You take a look outside of the wooden wagon, the bright light of the afternoon sun blinding you. It seems like you are currently on a big, grass covered field.")
-    print("You can't see anything other than the endless field of grass anywhere. What do you do?")
-elif wagon_choice == 3:
-    print("No one hears you.")
-    print(wagon_choice)
+# Intro
+name = input("Welcome to 'Unnamed'! Please enter your name to get started: ")
+print("Great! Welcome", name,"! Let's get started!")
+
+print("You wake up in an empty room. What happened? You don't remember. All you know is that you feel an immense pain in your head.")
+print("Current objective: Find a way out of this place")
+print("Tip: You will be presented with choices as you progress. Each choice will impact the story and your potential of getting out of this place.")
+print()
+print("There are three doors in front of you. All of them look identical, but your gut feeling tells you that only one of them will lead to the exit...")
+print("Which door do you choose?")
+print("All choices will be separated with '/'. Type the choice you want to make in the terminal.\nNOTE: You need to type exactly what it says in order for the game to register the choice as a valid one.")
+print("Which door do you choose?\n1/2/3")
+
+choices = input(">")
+
+if choices == "1":
+    print("You enter a plain room with the same grey stone walls as the last one. There is a lever at the back of the room. Do you pull it, or do you go back?")
+    print("yes/no")
+    print("Yes will pull the lever while no will make you go back to the previous room.")
+    
+    choices = input(">")
+    if choices == "yes":
+        print("You here the door slam shut behind you and a sound of water slowly filling the room you're currently standing in")
